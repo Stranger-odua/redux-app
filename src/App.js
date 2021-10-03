7704dbf52610530e25477268bc9113bcf7d92d57
Stranger <rooderuler@gmail.com>
2021-10-03 12:49:32 +0300

    1. Самый простой пример работы redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+
 
 function App() {
+  // чтобы как-то изменять состояние используем диспач, чтобы
+  // получить его внутри компонента юзаем хук useDispatch
+  const dispatch = useDispatch()
+  // чтобы получить состояние юзаем хук useSelector параметром он принимает функцию, а эта
+  // функция в свою очередь параметром принимает состояние
+  const cash = useSelector(state => state.cash)
+  console.log(cash)
+
+
+  const addCash = (cash) => {
+    // в dispatch передали action, то как action будет изменять состояние определено внутри reducer
+    dispatch({type:'ADD_CASH', payload: cash})
+  }
+
+  const getCash = (cash) => {
+    // в dispatch передали action, то как action будет изменять состояние определено внутри reducer
+    dispatch({type:'GET_CASH', payload: cash})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{fontSize: 28}}>{cash}</div>
+      <div style={{display: 'flex'}}>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+
+const defaultState = {
+  cash: 0,
+}
+
+// action = {type:'', payload:'?'}
+// поле type обязательное внутри action, второй элемент обычно называют payload
+// то как action будет изменять состояние определено внутри reducer
+
+// reducer принимает состояние и экшн
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      // возвращаем НОВЫЙ ОБЪЕКТ состояния
+      return {...state, cash: state.cash + action.payload}
+    case 'GET_CASH':
+      // возвращаем НОВЫЙ ОБЪЕКТ состояния
+      return {...state, cash: state.cash - action.payload}
+    default:
+      // ВСЕГДА ПО-УМОЛЧАНИЮ возвращаем старый объект состояния, если состояние никак не изменялось
+      return state
+  }
+}
+
+// reducer передается в store
+const store = createStore(reducer)
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  // store с помощью Provider передается в реакт компоненты
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  , document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
